7d6664bbf618897d165339780c4ee3c11d7ce5d8
tufan671 <65157420+tufan671@users.noreply.github.com>
2020-11-28 17:24:18 +0300

    Update 2-jail.js

--- komutlar/2-jail.js
+++ komutlar/2-jail.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 
 exports.run = async(client, message, args, ayar, emoji) => {
- if (!message.member.roles.cache.has("779644578445787136") && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor(0x00bfff).addField("Yetersiz Yetki",`Bu Komutu Kullanmak içi Yeterli Yetkiniz Yok`)).then(m => m.delete({timeout: 7000}));
+ if (!message.member.roles.cache.has("782247005769695252") && !message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor(0x00bfff).addField("Yetersiz Yetki",`Bu Komutu Kullanmak içi Yeterli Yetkiniz Yok`)).then(m => m.delete({timeout: 7000}));
   let uye = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
   let reason = args.splice(1).join(" ");
   if(!uye || !reason) return message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor(0x00bfff).setDescription("Geçerli bir üye ve sebep belirtmelisin!")).then(x => x.delete({timeout: 5000}));
   if (message.member.roles.highest.position <= uye.roles.highest.position) return message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor(0x00bfff).setDescription(`Belirttiğin kişi senden üstün veya onunla aynı yetkidesin!`)).then(x => x.delete({timeout: 5000}));
-  await uye.roles.set(uye.roles.cache.has("779311568486793227") ? ["779644631864311848", "779311568486793227"] : ["779644631864311848"]).catch();
+  await uye.roles.set(uye.roles.cache.has("782247851894898708") ? ["779644631864311848", "779311568486793227"] : ["779644631864311848"]).catch();
   if(uye.voice.channel) uye.voice.kick().catch();
   message.channel.send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor(0x00bfff).setDescription(`${uye} üyesi, ${message.author} tarafından **${reason}** nedeniyle jaile atıldı!`)).catch();
   client.channels.cache.get("779662103564517386").send(new Discord.MessageEmbed().setAuthor(message.member.displayName, message.author.avatarURL({dynamic: true})).setColor(0x00bfff).setDescription(`${uye} İsimli Kullanıcı Jail'e Atıldı\n\n• Yetkili <@!${message.author.id}> \`${message.author.id}\`\n• Kullanıcı: <@!${uye.id}> \`${uye.id}\`\n\n• Sebep: **${reason}**`)).catch();
